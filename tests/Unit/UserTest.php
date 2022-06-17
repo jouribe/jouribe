be8c805ab9892adbd39cf346c483c5a6d9a81b33
Jorge O. Uribe <jouribepe@gmail.com>
2022-06-17 04:03:35 -0500

    :wrench: UserTest rollback

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,9 +1,24 @@
 <?php /** @noinspection StaticClosureCanBeUsedInspection */
 
-uses(Tests\TestCase::class);
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-it('fetch a user', function () {
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+beforeEach(fn () => User::factory()->create());
+
+it('can create a user', function () {
+    $attributes = [
+        'name' => 'John Doe',
+        'email' => 'john@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ];
+
+    $response = $this->postJson('api/users', $attributes);
+    $response->assertStatus(201)->assertJson(['message' => 'User has been created']);
+
     $this->assertDatabaseHas('users', [
-        'email' => 'jorge@jouribe.dev',
+        'email' => $attributes['email'],
     ]);
 });
